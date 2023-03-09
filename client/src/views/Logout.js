6c5383f928063ac3befed69d5525ca0f5d5d1a00
Israel <arrx1380@msn.com>
2023-03-09 22:01:08 +0100

    Fixed logout

--- client/src/views/Logout.js
+++ client/src/views/Logout.js
@@ -1,19 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { clearUserAction } from "../actions/user";
-import { useHistory } from "react-router";
 
 const Logout = () => {
-  // History
-  const history = useHistory();
-
   // Dispatch
   const dispatch = useDispatch();
 
   // Effects
   useEffect(() => {
     dispatch(clearUserAction());
-    // history.push("/login");
   }, []);
 
   return <></>;
